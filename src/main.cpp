94d95148ced5e0f852a549c3093872061f350923
haiminh256 <haiminh2022012@gmail.com>
2026-06-14 09:31:01 +0700

    New level.csv format

--- src/main.cpp
+++ src/main.cpp
@@ -9,44 +9,6 @@ std::mt19937 gen(rd());
 std::uniform_int_distribution<> distr(0,2);
 int levelIndex = distr(gen);
 
-std::vector<std::vector<int>> level0 = {
-    {3,3,3,3,3,3,3,3,3,3,3,3,3,3}, 
-    {3,3,3,3,3,3,3,3,2,2,2,2,2,2}, 
-    {3,3,3,3,3,3,2,3,3,3,3,3,3,3}, 
-    {3,3,3,2,3,3,3,3,3,3,3,3,3,3},
-    {2,3,3,3,3,3,3,3,3,3,3,3,3,3},
-    {3,3,2,2,2,3,3,3,3,3,3,3,3,3}, 
-    {3,3,3,3,3,3,2,3,3,3,3,3,3,3},
-    {3,3,3,3,3,3,3,3,2,3,3,3,3,3},
-    {3,3,3,3,3,3,3,3,3,3,2,3,3,3},
-    {1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-};
-
-std::vector<std::vector<int>> level1 = {
-    {3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // Hàng 0 - ĐÍCH (Góc phải trên cùng)
-    {3,3,3,3,3,3,3,3,3,3,3,2,2,2}, // Hàng 1 - Bục cuối cùng để chạm đích
-    {3,3,3,3,3,3,3,3,2,2,2,3,3,3}, // Hàng 2
-    {3,3,3,3,3,2,2,3,3,3,3,3,3,3}, // Hàng 3 - Bục trung chuyển hướng sang phải
-    {2,2,2,3,3,3,3,3,3,3,3,3,3,3}, // Hàng 4 - Nhảy ngược về bên trái
-    {3,3,3,3,2,2,3,3,3,3,3,3,3,3}, // Hàng 5
-    {3,3,3,3,3,3,3,2,2,3,3,3,3,3}, // Hàng 6 - Bục đầu tiên khi từ sàn nhảy lên
-    {3,3,3,3,3,3,3,3,3,3,2,2,3,3}, // Hàng 7
-    {3,3,3,3,3,3,3,3,3,3,3,3,3,2}, // Hàng 8
-    {1,1,1,1,1,1,1,1,1,1,1,1,1,1}  // Hàng 9 - Vực thẳm ở giữa sàn! Rơi xuống là qua màn/reset
-};
-std::vector<std::vector<int>> level2 = {
-    {3,3,3,3,3,3,3,3,3,3,3,3,3,3}, // Hàng 0 - ĐÍCH (Góc phải trên cùng)
-    {3,3,3,3,3,3,3,3,3,3,3,3,2,2}, // Hàng 1 - Bục đích
-    {3,3,3,3,3,3,3,3,3,2,2,3,3,3}, // Hàng 2
-    {3,3,3,3,3,3,2,2,3,3,3,3,3,3}, // Hàng 3
-    {3,2,3,3,3,3,3,3,3,3,3,3,3,3}, // Hàng 4 - Các bục đá nhỏ dẫn lên
-    {3,2,2,3,3,3,3,3,3,3,3,3,3,3}, // Hàng 5
-    {3,3,3,3,3,2,2,3,3,3,3,3,3,3}, // Hàng 6 - Bục nhảy dài để lấy đà
-    {3,3,3,3,3,3,3,3,3,2,2,3,3,3}, // Hàng 7
-    {3,3,3,3,3,3,3,3,3,3,3,3,2,2}, // Hàng 8 - Vị trí xuất phát an toàn (100, 100) sẽ rơi trúng bục này
-    {1,1,1,1,1,1,1,1,1,1,1,1,1,1}  // Hàng 9 - Toàn bộ sàn biến thành VỰC THẲM!
-};
- 
 int main(int argc, char* argv[]) {
     Cosmos::Engine App;
     App.Init(SDL_INIT_VIDEO | SDL_INIT_VIDEO);
@@ -84,10 +46,10 @@ int main(int argc, char* argv[]) {
             levelIndex++;
             
             if (levelIndex == 1) {
-                map.Load(level1); 
+                map.LoadFromCSV("res/level1.csv"); 
             } 
             else if (levelIndex == 2) {
-                map.Load(level2); 
+                map.LoadFromCSV("res/level2.csv"); 
             }
             else {
                 levelIndex = 0;
